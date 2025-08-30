7f9b9d9c8b7e0220b5f871f8c2a3069c8a19d473
brqgoo <100725376+brqgoo@users.noreply.github.com>
2025-08-30 16:31:18 +0300

    OP_SHADOW_ALLOC_UP impl

--- src/executive/opcode/opcodes/shadowing/op_shadow_alloc_up.rs
+++ src/executive/opcode/opcodes/shadowing/op_shadow_alloc_up.rs
@@ -1,18 +1,74 @@
 use crate::{
-    executive::stack::{stack_error::StackError, stack_holder::StackHolder},
+    executive::stack::{
+        stack_error::{ShadowOpsError, StackError},
+        stack_holder::StackHolder,
+        stack_uint::{SafeConverter, StackItemUintExt},
+    },
     inscriptive::coin_holder::coin_holder::COIN_HOLDER,
 };
 
-/// Shadow allocation up.
+/// Increases the shadow space allocation of an account.
 #[derive(Debug, Clone, PartialEq, Eq)]
 #[allow(non_camel_case_types)]
 pub struct OP_SHADOW_ALLOC_UP;
 
 impl OP_SHADOW_ALLOC_UP {
-    pub fn execute(
-        _stack_holder: &mut StackHolder,
-        _coin_holder: &COIN_HOLDER,
+    pub async fn execute(
+        stack_holder: &mut StackHolder,
+        coin_holder: &COIN_HOLDER,
     ) -> Result<(), StackError> {
+        // If this is not the active execution, return immediately.
+        if !stack_holder.active_execution() {
+            return Ok(());
+        }
+
+        // Get the self contract id bytes.
+        let self_contract_id_bytes = stack_holder.contract_id();
+
+        // Pop the account key.
+        let account_key = stack_holder.pop()?;
+
+        // Convert the account key to bytes.
+        let account_key_bytes: [u8; 32] = match account_key.bytes().try_into() {
+            Ok(bytes) => bytes,
+            Err(_) => {
+                return Err(StackError::ShadowOpsError(
+                    ShadowOpsError::InvalidAccountKeyBytes(account_key.bytes().to_vec()),
+                ));
+            }
+        };
+
+        // Pop the amount.
+        let amount = stack_holder.pop()?;
+
+        // Convert the amount to a stack uint.
+        let amount_as_stack_uint = amount.to_stack_uint().ok_or(StackError::ShadowOpsError(
+            ShadowOpsError::InvalidAmountBytes(amount.bytes().to_vec()),
+        ))?;
+
+        // Convert the amount to a u64.
+        let amount_as_u64 = amount_as_stack_uint
+            .to_u64()
+            .ok_or(StackError::ShadowOpsError(
+                ShadowOpsError::InvalidAmountBytes(amount.bytes().to_vec()),
+            ))?;
+
+        // Get the contract coin holder.
+        let contract_coin_holder = {
+            let _coin_holder = coin_holder.lock().await;
+            _coin_holder.contract_coin_holder()
+        };
+
+        // Allocate the account key in the contract shadow space.
+        {
+            let mut _contract_coin_holder = contract_coin_holder.lock().await;
+            _contract_coin_holder
+                .shadow_alloc_up(self_contract_id_bytes, account_key_bytes, amount_as_u64)
+                .map_err(|error| ShadowOpsError::ShadowAllocUpError(error))
+                .map_err(StackError::ShadowOpsError)?;
+        }
+
+        // Return the result.
         Ok(())
     }
 
