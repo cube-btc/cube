3218a51fafb1358d637230cb993bfac7e2bc46a6
Burak <brock@bits.app>
2026-04-29 20:14:29 +0300

    PinlessSelf📌 txout type impl.

--- src/constructive/txout_types/pinless_self/mod.rs
+++ src/constructive/txout_types/pinless_self/mod.rs
@@ -1,1 +1,8 @@
-pub mod pinless_self;
+mod default;
+mod pinless_self;
+
+pub use default::{
+    return_pinless_self_default_scriptpubkey, return_pinless_self_default_taproot,
+    return_pinless_self_default_tapscript, PinlessSelfDefault,
+};
+pub use pinless_self::PinlessSelf;
--- src/constructive/txout_types/pinless_self/pinless_self/mod.rs
+++
@@ -1,8 +0,0 @@
-mod default;
-mod pinless_self;
-
-pub use default::{
-    return_pinless_self_default_scriptpubkey, return_pinless_self_default_taproot,
-    return_pinless_self_default_tapscript, PinlessSelfDefault,
-};
-pub use pinless_self::PinlessSelf;
